fac715f1940171c97ac96097e02f8a2daf829468
Hibi Miyazaki <hibi.miyazaki@gmail.com>
2018-09-26 10:52:26 +0900

    test

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
 
 // キャッシュ名とキャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
+var CACHE_NAME = 'pwa-sample-caches-2';
 var urlsToCache = [
 	'/pwa/',
 	'/pwa/index.html',
